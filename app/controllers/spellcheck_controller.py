2f474e29ebbe9ddb7074a2e6cbe39efb004ba8d1
antobbo <antobbo@gmail.com>
2024-12-26 14:02:58 +0000

    -add code to handle scan mode input as char and not number -add new error message -unit test to verify that

--- app/controllers/spellcheck_controller.py
+++ app/controllers/spellcheck_controller.py
@@ -2,6 +2,7 @@ class SpellcheckController:
 
     VALUE_CANNOT_BE_EMPTY_ERROR_MESSAGE = "The value of {element} cannot be empty."
     SCAN_MODE_WRONG_RANGE_ERROR_MESSAGE = "The range of the values should be between {first_value} and {last_value}."
+    VALUE_INVALID_TYPE_MUST_BE_NUMBER = "The value must be a number."
 
     def __init__(self, spellcheck_view, spellcheck_model):
         self.spellcheck_view = spellcheck_view
@@ -15,12 +16,15 @@ class SpellcheckController:
             errors.append(self.VALUE_CANNOT_BE_EMPTY_ERROR_MESSAGE.format(element=self.spellcheck_model.REPORT_FILE_STRING))
         if scan_mode == "":
             errors.append(self.VALUE_CANNOT_BE_EMPTY_ERROR_MESSAGE.format(element=self.spellcheck_model.SCAN_MODE_STRING))
-        if scan_mode != "" and int(scan_mode) <= 0 or int(scan_mode) > len(self.spellcheck_model.SCAN_MODE):
-            errors.append(self.SCAN_MODE_WRONG_RANGE_ERROR_MESSAGE.format(first_value=next(iter(self.spellcheck_model.SCAN_MODE)), last_value = len(self.spellcheck_model.SCAN_MODE)))
-
-
-
-
+        if scan_mode != "":
+            try:
+               scan_mode_opt =  int(scan_mode)
+               if scan_mode_opt <= 0 or int(scan_mode_opt) > len(self.spellcheck_model.SCAN_MODE):
+                   errors.append(self.SCAN_MODE_WRONG_RANGE_ERROR_MESSAGE.format(
+                       first_value=next(iter(self.spellcheck_model.SCAN_MODE)),
+                       last_value=len(self.spellcheck_model.SCAN_MODE)))
+            except ValueError:
+                errors.append(self.VALUE_INVALID_TYPE_MUST_BE_NUMBER)
         return errors
 
     def update_spellcheck_model(self, dictionary_file, report_file, scan_mode, file_types):
--- tests/test_controllers/test_spellcheck_controller.py
+++ tests/test_controllers/test_spellcheck_controller.py
@@ -13,6 +13,7 @@ class TestSpellCheckController(unittest.TestCase):
         view = SpellcheckView()
         self.controller = SpellcheckController(view, model)
 
+    #INPUT VALIDATION
     def test_should_validate_inputs_have_no_errors(self):
         dictionary_file = "C:\\Users\\Documents\\python\\Spellcheck_various\\dictionary.txt"
         report_file = "C:\\Users\\Documents\\python\\Spellcheck_various"
@@ -89,4 +90,11 @@ class TestSpellCheckController(unittest.TestCase):
         self.assertEqual(errors[1].__str__(), "The value of Report File cannot be empty.")
         self.assertEqual(errors[2].__str__(), "The range of the values should be between 1 and 2.")
 
-    #todo: test to deal with scan_mode = "a"
+    def test_should_scan_mode_be_number_error_message(self):
+        dictionary_file = "C:\\Users\\Documents\\python\\Spellcheck_various\\dictionary.txt"
+        report_file = "C:\\Users\\Documents\\python\\Spellcheck_various"
+        scan_mode = "test"
+        file_types = "doc, pdf"
+        errors = self.controller.validate_inputs(dictionary_file, report_file, scan_mode, file_types)
+        self.assertTrue(len(errors) == 1)
+        self.assertEqual(errors[0].__str__(), "The value must be a number.")
